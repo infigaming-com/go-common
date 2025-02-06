7e9401bd13e8446fdf8b8b6b1843cd0b28e9dd01
waynewu411 <waynewu411@gmail.com>
2025-02-06 13:46:46 +1100

    Move CalculateHmacSha256Hash() to util package and rename to HmacSha256Hash()

--- request/request_signer.go
+++ request/request_signer.go
@@ -2,11 +2,11 @@ package request
 
 import (
 	"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
 	"encoding/hex"
 	"io"
 	"net/http"
+
+	"github.com/infigaming-com/go-common/util"
 )
 
 func getCanonicalizedMessage(req *http.Request) []byte {
@@ -33,15 +33,8 @@ func getCanonicalizedMessage(req *http.Request) []byte {
 
 func HmacSha256Signer(req *http.Request, apiKeyHeader, apiKey, signatureHeader, apiKeySecret string) error {
 	canonicalizedMessage := getCanonicalizedMessage(req)
-	signature := CalculateHmacSha256Hash(canonicalizedMessage, apiKeySecret)
 	req.Header.Add(apiKeyHeader, apiKey)
-	req.Header.Add(signatureHeader, signature)
+	signature := util.HmacSha256Hash(canonicalizedMessage, []byte(apiKeySecret))
+	req.Header.Add(signatureHeader, hex.EncodeToString(signature))
 	return nil
-}
-
-func CalculateHmacSha256Hash(message []byte, secret string) string {
-	h := hmac.New(sha256.New, []byte(secret))
-	h.Write(message)
-	signatureBytes := h.Sum(nil)
-	return hex.EncodeToString(signatureBytes)
 }
