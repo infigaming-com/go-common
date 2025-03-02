e25925bcd53e02e564e3ffb2d436546c439a43b8
waynewu411 <waynewu411@gmail.com>
2025-03-02 21:30:16 +1100

    Delete secret

--- rate/furion_rate_provider_test.go
+++ rate/furion_rate_provider_test.go
@@ -13,8 +13,8 @@ func TestFurionRateProvider_TestGetRates(t *testing.T) {
 	provider := NewFurionRateProvider(
 		zap.NewNop(),
 		"http://localhost:8080/rates",
-		"hwostZ8GyYYBbHfa10MTmaGpUfQLfwLMb8hYX0jn9hH086yEgD7LdpkfR8MFbaZjQ2WFM05XXJrJPLDvu8870oT39jkw5yEWn9jOcT59LepQ9S9C9X8fXU4Fd9V3BQtA",
-		"ytUqDfrwJAFGwNdlz1b9ksVDF9xAk6Z7b6XZ80sEF7RZYPsQ9USAc5243L4r94EnqsJdBVU2bJTLzYuRYiYn8d5FtRNLuFA5WZugEgfWUb6ftPhjhgVsi3RLWn5gMcCv",
+		"",
+		"",
 	)
 	rates, err := provider.GetRates(context.Background(), []string{"USD", "USDT", "JPY"}, time.Now().Unix())
 	assert.NoError(t, err)
@@ -25,8 +25,8 @@ func TestFurionRateProvider_TestGetRate(t *testing.T) {
 	provider := NewFurionRateProvider(
 		zap.NewNop(),
 		"http://localhost:8080/rates",
-		"hwostZ8GyYYBbHfa10MTmaGpUfQLfwLMb8hYX0jn9hH086yEgD7LdpkfR8MFbaZjQ2WFM05XXJrJPLDvu8870oT39jkw5yEWn9jOcT59LepQ9S9C9X8fXU4Fd9V3BQtA",
-		"ytUqDfrwJAFGwNdlz1b9ksVDF9xAk6Z7b6XZ80sEF7RZYPsQ9USAc5243L4r94EnqsJdBVU2bJTLzYuRYiYn8d5FtRNLuFA5WZugEgfWUb6ftPhjhgVsi3RLWn5gMcCv",
+		"",
+		"",
 	)
 	rate, err := provider.GetRate(context.Background(), "USDT", time.Now().Unix())
 	assert.NoError(t, err)
