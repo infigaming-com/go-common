45e38224513d14d2d685c5f4bb6fce47b8e744d9
waynewu411 <waynewu411@gmail.com>
2025-05-29 21:53:49 +1000

    Update currency.go for k currency

--- util/currency.go
+++ util/currency.go
@@ -1,19 +1,36 @@
 package util
 
-import "strings"
+import (
+	"strings"
 
-// IsCurrencyWithK checks if the currency is a currency with (K) or (k) suffix
-func IsCurrencyWithK(currency string) bool {
-	return strings.HasSuffix(currency, "(K)") || strings.HasSuffix(currency, "(k)")
+	"github.com/samber/lo"
+)
+
+// IsKCurrency checks if the currency is a currency with (K) or (k) suffix
+func IsKCurrency(currency string) bool {
+	currency = strings.ToUpper(currency)
+	return strings.HasSuffix(currency, "(K)")
+}
+
+// NeedConvertToK checks if the currency needs to be converted to K
+func NeedConvertToKCurrency(currency string) bool {
+	return lo.Contains([]string{"VND", "IDR", "MMK"}, strings.ToUpper(currency))
+}
+
+// GetKCurrency returns the K currency of a currency
+func GetKCurrency(currency string) string {
+	currency = strings.ToUpper(currency)
+	if IsKCurrency(currency) {
+		return currency
+	}
+	return currency + "(K)"
 }
 
 // GetBaseCurrency returns the base currency of a currency with (K) or (k) suffix
 func GetBaseCurrency(currency string) string {
+	currency = strings.ToUpper(currency)
 	if strings.HasSuffix(currency, "(K)") {
 		return strings.TrimSuffix(currency, "(K)")
-	}
-	if strings.HasSuffix(currency, "(k)") {
-		return strings.TrimSuffix(currency, "(k)")
 	}
 	return currency
 }
